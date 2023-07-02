5fd3015015518515a8916b9f253f4b4c529bbf64
tarleak613 <134609299+tarleak613@users.noreply.github.com>
2023-07-03 00:19:00 +0530

    updated 21st june assignment

--- (6)14th June/tempCodeRunnerFile.java
+++
@@ -1,6 +0,0 @@
-   for (int j = 0; j < n; j++)
-                System.out.print(row[j] + " ");
-            System.out.println();
-            for (int j = 0; j < n; j++)
-                System.out.print(column[j] + " ");
-            System.out.println();
